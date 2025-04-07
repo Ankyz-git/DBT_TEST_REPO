cb3934d4ca92126063e1a51255b248df1764cadc
ankyz108@gmail.com <ankyz108@gmail.com>
2025-04-07 07:07:13 +0000

    commit test

--- Test_DBT_Model/dim_customer.sql
+++ Test_DBT_Model/dim_customer.sql
@@ -1,16 +1,11 @@
-
 {{ config(
-    materialized='table'  -- Ensures table creation in Snowflake
+    materialized='table',
+    schema='DBT_TEST_SCHEMA'
 ) }}
 
-CREATE OR REPLACE TABLE dim_customer as (
-    select
-    customer_id,
-    customer_name,
-    customer_segment,  -- Prevent NULL
-    customer_city
-    from dbt_test_db.dbt_test_schema.fact_sales     -- {{ source('dbt_test_schema', 'fact_sales') }}
-);
-
-
-
+SELECT
+  customer_id,
+  customer_name,
+  customer_segment,
+  customer_city
+FROM {{ source ('test_dbt_source', 'fact_sales') }}
---
+++ Test_DBT_Model/test.sql
@@ -0,0 +1,6 @@
+SELECT
+  customer_id,
+  customer_name,
+  customer_segment,
+  customer_city
+FROM dbt_test_db.DBT_TEST_SCHEMA.fact_sales
